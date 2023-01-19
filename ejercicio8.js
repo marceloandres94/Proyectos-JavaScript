399f74321cecca20919073de5a7b34630600c703
Marcelo Muñoz Manriquez <marceloandresmunozm@gmail.com>
2023-01-19 19:08:39 -0300

    partiendo la calculadora

---
+++ ejercicio8.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/* Hacer una calculadora:
+1.- que pida dos nuneros por pantalla
+2.- si metemos uno mal que nos lo vuelva a pedir
+3.- En el cuerpo de la pagina, en una alerta y por la consala poner el resultado de sumar,restar,multiplicar y dividir */
+
+var num1 = parseInt(prompt("Introduce el 1°mer numero: ",0));
+var num2 = parseInt(prompt("Introduce el 2°do numero: ",0));
+
